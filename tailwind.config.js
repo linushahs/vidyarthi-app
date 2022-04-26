0bbd30db0a0c5c081543b6f1ce491246fdc11e80
linushahs <suniltraveler2004@gmail.com>
2022-04-26 20:37:19 +0545

    Third Commit of Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,18 @@
 module.exports = {
-  content: [
-    "./src/**/*.js",
-    "./public/**/*.{html}"
-  ],
+  content: ["./src/**/*.js"],
   theme: {
     extend: {
-      boxShadow:{
-        'md': 'rgba(99, 99, 99, 0.15) 0px 2px 8px 0px;',
+      boxShadow: {
+        md: "rgba(99, 99, 99, 0.15) 0px 2px 8px 0px;",
       },
-      colors:{
-        current: '#6B8E4E',
-        'sec': '#F4F5F9', //secondary color
+      colors: {
+        current: "#6B8E4E",
+        sec: "#F4F5F9", //secondary color
       },
-      fontSize:{
-        '5xl':'52px',
-      }
+      fontSize: {
+        "5xl": "52px",
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/custom-forms'),
-  ],
-}
+  plugins: [require("@tailwindcss/custom-forms")],
+};
